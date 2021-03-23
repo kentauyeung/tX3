1a64db05ab15cebb8da53ecb1b8c1412340f8ffc
kent auyeung <kentayeung@gmail.com>
2021-03-23 13:01:07 -0700

    All cell toggle between two colors

--- js/index.js
+++ js/index.js
@@ -9,23 +9,59 @@ const cellEight = document.querySelector('.c8')
 const cellNine = document.querySelector('.c9')
 let count = 0
 
-cellOne.addEventListener('click', takeCell)
-cellTwo.addEventListener('click', takeCell)
-cellThree.addEventListener('click', takeCell)
-cellFour.addEventListener('click', takeCell)
-cellFive.addEventListener('click', takeCell)
-cellSix.addEventListener('click', takeCell)
-cellSeven.addEventListener('click', takeCell)
-cellEight.addEventListener('click', takeCell)
-cellNine.addEventListener('click', takeCell)
+cellOne.addEventListener('click', takeCellOne)
+cellTwo.addEventListener('click', takeCellTwo)
+cellThree.addEventListener('click', takeCellThree)
+cellFour.addEventListener('click', takeCellFour)
+cellFive.addEventListener('click', takeCellFive)
+cellSix.addEventListener('click', takeCellSix)
+cellSeven.addEventListener('click', takeCellSeven)
+cellEight.addEventListener('click', takeCellEight)
+cellNine.addEventListener('click', takeCellNine)
 
-function takeCell() {
+function takeCellOne() {
     count ++
-    if(isEven(count)) {
-        cellOne.style.background = 'red'
-    } else {
-        cellOne.style.background = 'blue'
-    }
+    isEven(count) ? cellOne.style.background = 'red' : cellOne.style.background = 'blue'
+}
+
+function takeCellTwo() {
+    count ++
+    isEven(count) ? cellTwo.style.background = 'red' : cellTwo.style.background = 'blue'
+}
+
+function takeCellThree() {
+    count ++
+    isEven(count) ? cellThree.style.background = 'red' : cellThree.style.background = 'blue'
+}
+
+function takeCellFour() {
+    count ++
+    isEven(count) ? cellFour.style.background = 'red' : cellFour.style.background = 'blue'
+}
+
+function takeCellFive() {
+    count ++ 
+    isEven(count) ? cellFive.style.background = 'red' : cellFive.style.background = 'blue'
+}
+
+function takeCellSix() {
+    count ++
+    isEven(count) ? cellSix.style.background = 'red' : cellSix.style.background = 'blue'
+}
+
+function takeCellSeven() {
+    count ++
+    isEven(count) ? cellSeven.style.background = 'red' : cellSeven.style.background = 'blue'
+}
+
+function takeCellEight() {
+    count ++
+    isEven(count) ? cellEight.style.background = 'red' : cellEight.style.background = 'blue'
+}
+
+function takeCellNine() {
+    count ++
+    isEven(count) ? cellNine.style.background = 'red' : cellNine.style.background = 'blue'
 }
 
 function isEven(n){
