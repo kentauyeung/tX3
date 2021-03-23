36886a7f8e843651f1b7a0a8bb180c4e2773b570
kent auyeung <kentayeung@gmail.com>
2021-03-23 12:30:45 -0700

    Created variables for cells

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const cellOne = document.querySelector('.c1')
+const cellTwo = document.querySelector('.c2')
+const cellThree = document.querySelector('.c3')
+const cellFour = document.querySelector('.c4')
+const cellFive = document.querySelector('.c5')
+const cellSix = document.querySelector('.c6')
+const cellSeven = document.querySelector('.c7')
+const cellEight = document.querySelector('.c8')
+const cellNine = document.querySelector('.c9')
+
+cellOne.addEventListener('click', takeCell)
+
+function takeCell() {
+    cell.style.background = 'red'
+}
+
+function isEven(){
+    let count = 0
+    if( //something happens, add 1 ) {
+        return count % 2 === 0
+    }
+}
