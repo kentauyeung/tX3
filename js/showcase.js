d075f3f0c104a625b3ca58ff3da4c2ef968518d8
kent auyeung <kentayeung@gmail.com>
2021-03-24 20:56:11 -0700

    Added showcase page

---
+++ js/showcase.js
@@ -0,0 +1,5 @@
+document.querySelector('.cell-display-ex').addEventListener('click', toggleColor)
+
+function toggleColor() {
+    document.querySelector('.cell-display-ex').classList.toggle('toggleRed')
+}
